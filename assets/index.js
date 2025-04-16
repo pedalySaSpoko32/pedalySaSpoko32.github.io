ea305cd5a2d43f65afe3de373a11d74e13385f68
pedalySaSpoko32 <obywatelcweli@gmail.com>
2025-04-16 15:08:42 +0000

    Update index.js

--- assets/index.js
+++ assets/index.js
@@ -57,7 +57,7 @@ imageInput.addEventListener('change', (event) => {
     fetch('	https://api.imgur.com/3/image' ,{
         method: 'POST',
         headers: {
-            'Authorization': 'Client-ID 4ecc257cbb25ccc'
+            'Authorization': 'Client-ID 95e5303d3faca98'
         },
         body: data
     })
